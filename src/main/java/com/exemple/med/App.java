f52fcc6d3c6acf7b525b8ac123f6e251e99a52c6
09401788 <ghoulem.mohamed@gmail.com>
2019-11-05 23:14:15 -1200

    acsess

---
+++ src/main/java/com/exemple/med/App.java
@@ -0,0 +1,16 @@
+package com.exemple.med;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( args[0] );
+
+
+    }
+}
